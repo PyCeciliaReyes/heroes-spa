339bfedfd8ce99db435c6e291bdf4decd80ec008
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-09 16:29:15 -0400

    Clase: Creando un segundo Router
    Se da dos vistas diferentes. No se visualiza el navbar estando en /login, y se crea 2 rutas mas

---
+++ src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,5 @@
+export const HeroPage = () => {
+    return(
+        <h3>HeroPage</h3>
+    );
+};
---
+++ src/heroes/pages/SearchPage.jsx
@@ -0,0 +1,5 @@
+export const SearchPage = () => {
+    return(
+        <h3>SearchPage</h3>
+    );
+};
