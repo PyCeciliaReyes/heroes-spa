f50b54336b343e87fb4743f9b0e068c29a404f40
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-11 09:44:26 -0400

    Clase: diseño de la pantalla de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,45 @@
+import { HeroCard } from "../components";
+
 export const SearchPage = () => {
     return(
-        <h3>SearchPage</h3>
+        <>
+            <h1>SearchPage</h1>
+            <hr />
+            <div className="row">
+
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form action="">
+                        <input 
+                            type="text"
+                            placeholder="Search a hero"
+                            className="form-control"
+                            name='searchText'
+                            autoComplete="off"
+                        />
+                        <button className="btn btn-outline-primary mt-2">
+                            Search
+                        </button>
+                    </form>
+                </div>
+
+                <div className="col-7">
+                    <h4>Results</h4>
+                    <hr />
+                    <div className="alert alert-primary">
+                        Search a hero
+                    </div>
+
+                    <div className="alert alert-danger">
+                        No hero with ABC
+                    </div>
+
+                    {/* <HeroCard /> */}
+
+
+                </div>
+            </div>
+        </>
     );
 };
