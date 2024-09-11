0dc051740177427c838daaeb349d5355bf78d5a8
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-11 12:54:46 -0400

    Clase: login de un usuario
    Continuacion de la configuracion para el login de un usuario y poder visualizar en el menu principal

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authRedudcer";
+import { types } from "../types/types";
 
 
 const initialState = {
@@ -11,8 +12,22 @@ export const AuthProvider = ({children}) => {
     
     const [authSate, dispatch] = useReducer(authReducer, initialState);
 
+    const login = (name = '') => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 'ABC',
+                name: name
+            }
+        }
+        dispatch(action);
+    };
+
     return(
-        <AuthContext.Provider value={{ }}>
+        <AuthContext.Provider value={{ 
+            ...authSate,
+            login:login 
+        }}>
             {children}
         </AuthContext.Provider>
     );
