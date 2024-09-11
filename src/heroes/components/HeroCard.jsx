1f2fc8bdf8c5989abba3a0fbaac27dc0695e0b07
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-11 11:06:20 -0400

    Se cambia el path de HeroCard y HeroPage para utilizar en produccion y en desarrollo

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -14,7 +14,7 @@ export const HeroCard = ({
         characters,
     }) => {
         
-    const heroImageUrl = `./public/heroes/${ id }.jpg`;
+    const heroImageUrl = `/heroes/${ id }.jpg`;
 
 
     return(
